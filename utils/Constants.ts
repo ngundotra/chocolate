bf9aa990348b613f769c14ddf9a878efe29f67e3
Edison Moy <moyedison@gmail.com>
2021-09-04 16:29:03 -0400

    add program id pubkey

--- utils/Constants.ts
+++ utils/Constants.ts
@@ -1,7 +1,8 @@
-import {
-    PublicKey
-} from '@solana/web3.js';
+import { PublicKey } from "@solana/web3.js";
 
 // export const ADDR = "5Ef6TYJvEsNGFH7rhARyGgVutSRCWz5czfbNb7Wsyab";
 export const ADDR = "GZbQmKYYzwjP3nbdqRWPLn98ipAni9w5eXMGp7bmZbGB";
 export const PUBKEY = new PublicKey(ADDR);
+export const TOKEN_PROGRAM_ID = new PublicKey(
+    "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
+);
