ecfaeb3a049cbbeabc420ba962c7ad256c27b38a
Noah Gundotra <noah@gundotra.org>
2021-09-05 18:21:37 -0400

    add ui work

---
+++ ui/src/CollectiblesView.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+import {
+    Box,
+    Grid,
+    Image
+} from '@chakra-ui/react';
+
+export function CollectiblesView() {
+    return (
+        <Box boxSize="sm" justifySelf='center'>
+            <Image src="https://arweave.net/EDAgZZ-6sZ8aK-awNTtG2qk3R1kVlV1c5_JACf8GDvs" />
+        </Box>
+    )
+}
---
+++ ui/src/ProfilePage.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+import {
+    Box,
+    Grid,
+    Image
+} from '@chakra-ui/react';
+
+import { CollectiblesView } from "./CollectiblesView";
+import { FungiblesView } from './FungiblesView';
+import { NFT_ADDR } from "./utils/Constants";
+
+export function ProfilePage() {
+
+    return (
+      <Grid minH="100vh" p={3}>
+        <Box marginTop="50px">
+          <h1>Pubkey: {NFT_ADDR.toString()}</h1>
+        </Box>
+        <CollectiblesView />
+        <FungiblesView />
+      </Grid>
+    )
+}
